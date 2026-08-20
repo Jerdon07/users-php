bb746e860f71909c570f7c4916a81b742da93917
Jerdon07 <jdlitaoen@gmail.com>
2026-08-20 16:58:13 +0800

    FEAT: Create view for about

---
+++ views/about.view.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+    <title><?= $heading ?></title>
+</head>
+<body id="body">
+    <?php require 'views/components/nav.php' ?>
+
+    <main class="main">
+        <div class="card">
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Actions</th>
+                </thead>
+                <tr>
+                    <td>Tocmol</td>
+                    <td>Video Engineer</td>
+                    <td>
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
